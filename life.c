d565c91883737331c1ff12a3259046ba6f4ec310
nasyrovdp <nasyrovdp@yandex.ru>
2021-10-26 10:56:19 +0300

    changed the logic for drawing borders

--- life.c
+++ life.c
@@ -1,53 +1,24 @@
 #include <stdio.h>
 
 int main () {
-    char coordinates[11][11];
+    char coordinates[10][10];
     int i;
     int j;
 
-    /* fill the field with spaces */
-    for (i = 0; i < 11; i++) {
-        for (j = 0; j < 11; j++) {
-            coordinates[i][j] = ' ';
+    /* fill the field with spaces and set borders */
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            if ((i == 0 || i == 9) && (j > 0 && j < 9)) {
+                coordinates[i][j] = j + '0';
+            }
+            else if ((j == 0 || j == 9) && (i > 0 && i < 9)) {
+                coordinates[i][j] = i + '0';
+            }
+            else {
+                coordinates[i][j] = ' ';
+            }
         }
     }
-
-    /* set the border */
-    coordinates[0][1] = '1';
-    coordinates[0][2] = '2';
-    coordinates[0][3] = '3';
-    coordinates[0][4] = '4';
-    coordinates[0][5] = '5';
-    coordinates[0][6] = '6';
-    coordinates[0][7] = '7';
-    coordinates[0][8] = '8';
-
-    coordinates[9][1] = '1';
-    coordinates[9][2] = '2';
-    coordinates[9][3] = '3';
-    coordinates[9][4] = '4';
-    coordinates[9][5] = '5';
-    coordinates[9][6] = '6';
-    coordinates[9][7] = '7';
-    coordinates[9][8] = '8';
-
-    coordinates[1][0] = '1';
-    coordinates[2][0] = '2';
-    coordinates[3][0] = '3';
-    coordinates[4][0] = '4';
-    coordinates[5][0] = '5';
-    coordinates[6][0] = '6';
-    coordinates[7][0] = '7';
-    coordinates[8][0] = '8';
-
-    coordinates[1][9] = '1';
-    coordinates[2][9] = '2';
-    coordinates[3][9] = '3';
-    coordinates[4][9] = '4';
-    coordinates[5][9] = '5';
-    coordinates[6][9] = '6';
-    coordinates[7][9] = '7';
-    coordinates[8][9] = '8';
 
     /* set the seed */
     coordinates[3][3] = 'O';
@@ -57,8 +28,8 @@ int main () {
     coordinates[5][4] = 'O';
 
     /* print the whole field */
-    for(i = 0; i < 11; i++) {
-        for (j = 0; j < 11; j++) {
+    for(i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
             printf("%c ", coordinates[i][j]);
         }
         puts("");
