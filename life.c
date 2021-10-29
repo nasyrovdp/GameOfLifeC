80d3fbecf8637cadb97965e7e559b1b8889f3f72
nasyrovdp <nasyrovdp@yandex.ru>
2021-10-29 14:59:18 +0300

    Still working on the basic functionality

--- life.c
+++ life.c
@@ -1,51 +1,88 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main () {
-    int y = 10;
-    int x = 10;
-    char coordinates[y][x];
+int y = 7;
+int x = 7;
+char coordinates[7][7];
 
-    /* fill the field with spaces and set borders */
+/* print the whole field */
+void print_field() {
     for (short i = 0; i < y; i++) {
         for (short j = 0; j < x; j++) {
-            if ((i == 0 || i == y - 1) && (j > 0 && j < y - 1)) {
-                coordinates[i][j] = j + '0';
-            }
-            else if ((j == 0 || j == y - 1) && (i > 0 && i < y - 1)) {
-                coordinates[i][j] = i + '0';
-            }
-            else {
-                coordinates[i][j] = ' ';
-            }
+            printf("%c ", coordinates[i][j]);
+        }
+        puts("");
+    }
+}
+
+/* count the neighbor live cells */
+int neighbor_check(int y, int x) {
+    int neighbors_count = 0;
+    // for (short i = y-1; i <= y+1; i++) {
+    //     for (short j = x-1; j <= x+1; j++) {
+    //         if (coordinates[i][j] == 'o') {
+    //             neighbors_count++;
+    //         }
+    //     }
+    // }
+    if (coordinates[y-1][x-1] == 'o') {
+        neighbors_count++;
+    }
+    if (coordinates[y-1][x] == 'o') {
+        neighbors_count++;
+    }
+    if (coordinates[y-1][x+1] == 'o') {
+        neighbors_count++;
+    }
+    if (coordinates[y][x-1] == 'o') {
+        neighbors_count++;
+    }
+    if (coordinates[y][x+1] == 'o') {
+        neighbors_count++;
+    }
+    if (coordinates[y+1][x-1] == 'o') {
+        neighbors_count++;
+    }
+    if (coordinates[y+1][x] == 'o') {
+        neighbors_count++;
+    }
+    if (coordinates[y+1][x+1] == 'o') {
+        neighbors_count++;
+    }
+    return neighbors_count;
+}
+
+int main () {
+
+    /* fill the field with dots */
+    for (short i = 0; i < y; i++) {
+        for (short j = 0; j < x; j++) {
+            coordinates[i][j] = '.';
         }
     }
 
     /* set the seed */
-    coordinates[4][3] = 'O';
-    coordinates[4][4] = 'O';
-    coordinates[4][5] = 'O';
+    char a = coordinates[y/2][x/2-1] = 'o';
+    char b = coordinates[y/2][x/2] = 'o';
+    char c = coordinates[y/2][x/2+1] = 'o';
 
-    /* print the whole field */
+    print_field();
+
+    int cell = neighbor_check(2, 3);
+    printf("%i\n", cell);
+
     for (short i = 0; i < y; i++) {
         for (short j = 0; j < x; j++) {
-            printf("%c ", coordinates[i][j]);
-        }
-        puts("");
-    }
-
-    /* check a cell for neighbors */
-    int neighbors_count = 0;
-    for (short i = 0; i < y; i++) {
-        for (short j = 0; j < x; j++) {
-            if (coordinates[i][j] != ' ') {
-                for (short i = -1; i < 1; i++) {
-                  /* code */
-                }
+            short z = neighbor_check(i, j);
+            if (coordinates[i][j] == 'o' && (z <= 1 || z > 3)) {
+                coordinates[i][j] = '.';
+            } else if (coordinates[i][j] == '.' && z == 3) {
+                coordinates[i][j] = 'o';
             }
-            printf("%c ", coordinates[i][j]);
         }
-        puts("");
     }
+
+    print_field();
 
     return 0;
 }
