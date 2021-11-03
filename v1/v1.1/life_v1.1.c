103246e65333f01e74df44b6e2ffc949eab97bf6
nasyrovdp <nasyrovdp@yandex.ru>
2021-11-03 17:58:37 +0300

    moved everything to functions and debugged

--- v1/v1.1/life_v1.1.c
+++ v1/v1.1/life_v1.1.c
@@ -10,8 +10,8 @@ char coordinates_future[SIZE][SIZE];
 
 void print_field() {
     /* print the whole field */
-    for (short i = 0; i < y; i++) {
-        for (short j = 0; j < x; j++) {
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
             printf("%c ", coordinates[i][j]);
         }
         puts("");
@@ -19,86 +19,92 @@ void print_field() {
 }
 
 void fill_current_field() {
-    /* fill the field with dots */
-    for (short i = 0; i < y; i++) {
-        for (short j = 0; j < x; j++) {
+    /* fill the field with spaces */
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
             coordinates[i][j] = ' ';
         }
     }
 }
 
 void fill_future_field() {
-    /* fill the future field with dots */
-    for (short i = 0; i < y; i++) {
-        for (short j = 0; j < x; j++) {
+    /* fill the future field with spaces */
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
             coordinates_future[i][j] = ' ';
         }
     }
 }
 
-int neighbor_check(int y, int x) {
-    /* count the neighbor live cells */
+int count_neighbors(int cell_y, int cell_x) {
     int neighbors_count = 0;
-    for (short i = y-1; i <= y+1; i++) {
-        for (short j = x-1; j <= x+1; j++) {
+    for (int i = cell_y-1; i <= cell_y + 1; i++) {
+        for (int j = cell_x-1; j <= cell_x + 1; j++) {
             if (coordinates[i][j] == 'o') {
                 neighbors_count++;
             }
         }
     }
-    if (coordinates[y][x] == 'o') {
+    if (coordinates[cell_y][cell_x] == 'o') {
         neighbors_count--;
     }
     return neighbors_count;
 }
 
-int main () {
+void make_move() {
+    for (int i = 1; i < y; i++) {
+        for (int j = 1; j < x; j++) {
+            if (coordinates[i][j] == 'o' && (count_neighbors(i, j) < 2 || count_neighbors(i, j) > 3)) {
+                coordinates_future[i][j] = ' ';
+            } else if (coordinates[i][j] == ' ' && count_neighbors(i, j) == 3) {
+                coordinates_future[i][j] = 'o';
+            } else if (coordinates[i][j] == 'o' && (count_neighbors(i, j) == 2 || count_neighbors(i, j) == 3)) {
+                coordinates_future[i][j] = 'o';
+            }
+        }
+    }
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            coordinates[i][j] = coordinates_future[i][j];
+        }
+    }
+}
 
+// int neighbor_check(int y, int x) {
+//     /* count the neighbor live cells */
+//     int neighbors_count = 0;
+//     for (int i = y-1; i <= y+1; i++) {
+//         for (int j = x-1; j <= x+1; j++) {
+//             if (coordinates[i][j] == 'o') {
+//                 neighbors_count++;
+//             }
+//         }
+//     }
+//     if (coordinates[y][x] == 'o') {
+//         neighbors_count--;
+//     }
+//     return neighbors_count;
+// }
+
+int main () {
     fill_current_field();
     fill_future_field();
 
     /* set the seed */
-    coordinates[y/2][x/2-5] = 'o';
-    coordinates[y/2][x/2-4] = 'o';
-    coordinates[y/2][x/2-3] = 'o';
-    coordinates[y/2][x/2-2] = 'o';
-    coordinates[y/2][x/2-1] = 'o';
-    coordinates[y/2][x/2] = 'o';
-    coordinates[y/2][x/2+1] = 'o';
-    coordinates[y/2][x/2+2] = 'o';
-    coordinates[y/2][x/2+3] = 'o';
-    coordinates[y/2][x/2+4] = 'o';
+    for (int i = -5; i <= 4; i++) {
+        coordinates[y/2][x/2+i] = 'o';
+    }
 
     print_field();
+    usleep(1000000);
 
-    for (short i = 0; i < 50; i++) {
-
-        /* check the whole field for future changes */
-        for (short i = 0; i < y; i++) {
-            for (short j = 0; j < x; j++) {
-                short z = neighbor_check(i, j);
-                if (coordinates[i][j] == 'o' && (z < 2 || z > 3)) {
-                    coordinates_future[i][j] = ' ';
-                } else if (coordinates[i][j] == 'o' && (z == 2 || z == 3)) {
-                    coordinates_future[i][j] = 'o';
-                } else if (coordinates[i][j] == ' ' && z == 3) {
-                    coordinates_future[i][j] = 'o';
-                }
-            }
-        }
-
-        /* apply the changes to current field */
-        for (short i = 0; i < y; i++) {
-            for (short j = 0; j < x; j++) {
-                coordinates[i][j] = coordinates_future[i][j];
-            }
-        }
-
-        usleep(500000);
-
-        puts("");
-        printf("    step %i\n", i + 1);
+    // printf("%i\n", count_neighbors(1, 1));
+    for (int i = 0; i < 50; i++) {
+        make_move();
+        printf("    step %i:\n", i + 1);
         print_field();
+
+        usleep(300000);
     }
 
     return 0;
