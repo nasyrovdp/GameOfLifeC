a62571f78237a894a035a11a6f5625f16b082170
nasyrovdp <nasyrovdp@yandex.ru>
2021-11-03 11:28:18 +0300

    added v2.0

---
+++ v2.0/life_v2.0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    /* set the field size */
+    /* fill the field with dots/spaces */
+    /* set the seed */
+    /* print the field with the seed */
+    /* count neighbors of every cell and store changes in the future field */
+    /* apply changes to the current field and print the field */
+    /* repeat steps 8 and 9 */
+
+    return 0;
+}
